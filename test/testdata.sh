ffb0ac9cfe14d67455e19bdbfdd760a3c01df51f
stevengogogo <stevengogogo4321@gmail.com>
2021-06-16 13:50:30 +0000

    🎉 All data passed

--- test/testdata.sh
+++ test/testdata.sh
@@ -3,7 +3,7 @@
 echo ""
 echo "$(tput setaf 2; tput bold)Start test: Input/Output $(tput sgr0)"
 
-for i in {1..5};
+for i in {1..13};
 do
     echo ""
     echo "$(tput setaf 5)Test File: $i.in$(tput sgr0)";
